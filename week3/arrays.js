08709081a19de4e775a9f841aa70cf076b7128c6
helloprof <suluxan.seneca@gmail.com>
2023-05-26 15:11:00 -0400

    arrays code

---
+++ week3/arrays.js
@@ -0,0 +1,17 @@
+let myList = ["apples","oranges","bananas","mangoes"]
+
+// console.log(myList[0])
+
+
+for(let i = 0; i < myList.length; i++) {
+    console.log(myList[i])
+}
+// myList.find()
+myList.forEach(function(e) {
+    console.log(e) 
+})
+// myList.forEach(applyFilter)
+
+// function applyFilter(e) {
+//     console.log(e)
+// }
